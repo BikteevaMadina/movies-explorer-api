227f4687d24b2453d735adc3be04b4c1871f2f5b
Madina Bikteeva <112384268+BikteevaMadina@users.noreply.github.com>
2023-06-26 13:51:14 +0500

    rename mongo

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
-  MONGO_URL: process.env.MONGODB || 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb',
 };
